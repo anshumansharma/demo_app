1e6b3ed801b51756573524a95dd95cdef991aad7
Anshuman Sharma <anshuman@ubiqihealth.com>
2012-06-20 10:54:13 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = 'dfe0a0daac7206fac7c48e85e6459fab56a500c55d79adcdddacad4b51b8082c98b5dae80b5561380c0e14789219ffdf92d8c7fcbf6641d46e6a15ece726f45d'
